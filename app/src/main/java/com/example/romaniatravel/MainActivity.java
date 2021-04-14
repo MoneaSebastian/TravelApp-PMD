204b24bdb40ab51a15b3b1519d37d173c739d738
unknown <sebastian.monea00@e-uvt.ro>
2021-04-14 13:46:42 +0300

    first version - UI first page

---
+++ app/src/main/java/com/example/romaniatravel/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.romaniatravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Paltinis(View view) {
+        Toast.makeText(this, "You have clicked Paltinis", Toast.LENGTH_LONG).show();
+    }
+
+    public void Ranca(View view) {
+        Toast.makeText(this, "You have clicked Ranca", Toast.LENGTH_LONG).show();
+    }
+
+    public void Sinaia(View view) {
+        Toast.makeText(this, "You have clicked Sinaia", Toast.LENGTH_LONG).show();
+    }
+
+    public void Straja(View view) {
+        Toast.makeText(this, "You have clicked Straja", Toast.LENGTH_LONG).show();
+    }
+
+    public void Sureanu(View view) {
+        Toast.makeText(this, "You have clicked Sureanu", Toast.LENGTH_LONG).show();
+    }
+
+    public void Vartop(View view) {
+        Toast.makeText(this, "You have clicked Vartop", Toast.LENGTH_LONG).show();
+    }
+}
